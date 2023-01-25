6227ef719c7ade64d2bda9a98897c9a760a0528f
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-25 16:07:57 -0700

    Code Along

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+  create an array from the input string
+  reverse the array
+  create a string from the reversed array
+  return the reversed string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
@@ -12,12 +29,31 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
+ 
   console.log("");
-
+ 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+ 
+  console.log("");
+ 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+ 
+  console.log("");
+ 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+ 
+  console.log("");
+ 
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+ 
+  console.log("");
+ 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
